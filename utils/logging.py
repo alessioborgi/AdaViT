577beb3d50fddedd127cfe6a9d2de0cbeef7d522
AlessioBorgi <borgi.1952442@studenti.uniroma1.it>
2024-03-30 07:02:50 +0000

    Trying to modify configs parameters.

--- utils/logging.py
+++ utils/logging.py
@@ -2,6 +2,7 @@ import os
 import wandb
 from pprint import pprint
 import time
+import yaml
 
 last_print_time = 0
 
@@ -41,7 +42,7 @@ class WandbLogger:
     """
     Logger for logging to wandb.
     """
-    def __init__(self, wandb_entity, wandb_project, settings, dir=None, wandb_run=None):
+    def __init__(self, wandb_entity, wandb_project, settings, train_config_path, dir=None, wandb_run=None):
         self.entity = wandb_entity
         self.project = wandb_project
         self.wandb_run = wandb_run
@@ -55,9 +56,13 @@ class WandbLogger:
             name=wandb_run,
             dir=dir,
         )
+
+    settings = gather_settings_train(self.train_config_path)
+    print("The settings is: ", settings)
+    
     wandb.init(
     # set the wandb project where this run will be logged
-    project="my-awesome-project",
+    project="Laplacian_AdaViT",
 
     # track hyperparameters and run metadata
     config={
@@ -73,3 +78,29 @@ class WandbLogger:
     
     def close(self):
         wandb.finish()
+    
+    def gather_settings_train(train_config_path):
+        settings = {}
+
+        # Load contents of train_config.yaml into settings
+        with open(train_config_path, "r") as file:
+            train_config_dict = yaml.safe_load(file)
+            settings.update(train_config_dict)
+
+        # Helper function to recursively gather settings from defaults
+        def gather_defaults(defaults_list):
+            for default in defaults_list:
+                if isinstance(default, str):
+                    # If default is a string, assume it's a path to a YAML file
+                    default_path = os.path.join(os.path.dirname(train_config_path), default)
+                    with open(default_path, "r") as file:
+                        default_dict = yaml.safe_load(file)
+                        settings.update(default_dict)
+                elif isinstance(default, dict):
+                    # If default is a dictionary, recursively gather its settings
+                    gather_defaults(default.values())
+
+        gather_defaults(settings.get("defaults", []))
+
+        return settings
+    
